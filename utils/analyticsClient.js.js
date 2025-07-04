4d76d649116e97ca8fad9590a16048ef610291b3
sonusatish34 <sonusatish34@gmail.com>
2025-07-05 00:01:52 +0530

    dw

--- utils/analyticsClient.js.js
+++ utils/analyticsClient.js.js
@@ -4,7 +4,7 @@ const { BetaAnalyticsDataClient } = require('@google-analytics/data');
 const analyticsDataClient = new BetaAnalyticsDataClient({
   credentials: {
     client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL_LDC,
-    private_key: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL_LDC?.replace(/\\n/g, '\n'),
+    private_key: process.env.REACT_APP_API_KEY?.replace(/\\n/g, '\n'),
   },
 });
 
