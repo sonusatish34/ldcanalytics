7ee2338fceb601790557b44540bf2aa14311bad2
sonusatish34 <sonusatish34@gmail.com>
2025-07-05 00:13:47 +0530

    fe

--- api/analytics/realtime.js
+++ api/analytics/realtime.js
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
         { name: 'country' },
       ],
       metrics: [{ name: 'activeUsers' }],
+      // dw
     });
 
     res.status(200).json(response);
